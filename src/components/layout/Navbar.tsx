cefcbfb1717503f606cadcf025508b24bb4bb9f9
Shah Ali Hridoy <shahalihridoy@gmail.com>
2023-04-15 03:02:20 +0600

    Update packages

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,13 +1,8 @@
-import NextImage from "@components/atoms/NextImage";
-import NextLink from "@components/atoms/NextLink";
 import { ArrowDropDown } from "@mui/icons-material";
-import { Button, Card, Container, MenuItem } from "@mui/material";
+import { Box, Button, Card, Container } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
-import { Box } from "@mui/material";
 import { NAVBAR_HEIGHT } from "@shared/constants";
 import navbarNavigations from "@shared/navbarNavigations";
-import clsx from "clsx";
-import React from "react";
 import StyledNavLinks from "./NavbarLinks.style";
 import Sidenav from "./Sidenav";
 
@@ -27,73 +22,75 @@ const Navbar = () => {
           height: NAVBAR_HEIGHT,
         }}
       >
-        <NextLink href="/" sx={{ lineHeight: 1 }}>
+        {/* <NextLink href="/" sx={{ lineHeight: 1 }}>
           <NextImage
             src="/assets/trackem-logo-full.svg"
             width={176}
             height={45}
           />
-        </NextLink>
+        </NextLink> */}
 
         <Sidenav />
 
         <StyledNavLinks>
-          {navbarNavigations.map((item) =>
-            item.children ? (
-              <Box
-                position="relative"
-                key={item.title}
-                sx={{
-                  "&:hover": { "& .dropdown-menu": { display: "block" } },
-                }}
-              >
-                <Button
-                  className="hover-bg-none"
-                  sx={{ px: "0.5rem", mr: "1.5rem" }}
-                >
-                  {item.title}
-                  <ArrowDropDown fontSize="small" sx={{ ml: "2px" }} />
-                </Button>
+          {navbarNavigations.map(
+            (item) =>
+              item.children ? (
                 <Box
-                  className="dropdown-menu"
-                  display="none"
-                  position="absolute"
-                  top="100%"
+                  position="relative"
+                  key={item.title}
+                  sx={{
+                    "&:hover": { "& .dropdown-menu": { display: "block" } },
+                  }}
                 >
-                  <Card
-                    elevation={6}
-                    sx={{ minWidth: 120, marginTop: "0.8rem", py: "0.25rem" }}
+                  <Button
+                    className="hover-bg-none"
+                    sx={{ px: "0.5rem", mr: "1.5rem" }}
                   >
-                    {item.children.map((child) => (
+                    {item.title}
+                    <ArrowDropDown fontSize="small" sx={{ ml: "2px" }} />
+                  </Button>
+                  <Box
+                    className="dropdown-menu"
+                    display="none"
+                    position="absolute"
+                    top="100%"
+                  >
+                    <Card
+                      elevation={6}
+                      sx={{ minWidth: 120, marginTop: "0.8rem", py: "0.25rem" }}
+                    >
+                      {/* {item.children.map((child) => (
                       <NextLink href={`${child.url}`} key={child.title}>
                         <MenuItem>{child.title}</MenuItem>
                       </NextLink>
-                    ))}
-                  </Card>
+                    ))} */}
+                    </Card>
+                  </Box>
                 </Box>
-              </Box>
-            ) : (
-              <NextLink
-                href={item.url || "/"}
-                activeClass="nav-active"
-                key={item.title}
-                exact
-                sx={{ mr: !item.outlined ? "1.5rem" : 0 }}
-              >
-                <Button
-                  className={clsx({
-                    "hover-effect": !item.outlined,
-                    "hover-bg-none": !item.outlined,
-                  })}
-                  sx={{
-                    position: "relative",
-                    bgcolor: item.outlined ? "grey.100" : "transparent",
-                  }}
-                >
-                  {item.title}
-                </Button>
-              </NextLink>
-            ),
+              ) : null,
+            // (
+            //   <NextLink
+            //     href={item.url || "/"}
+            //     activeClass="nav-active"
+            //     key={item.title}
+            //     exact
+            //     sx={{ mr: !item.outlined ? "1.5rem" : 0 }}
+            //   >
+            //     <Button
+            //       className={clsx({
+            //         "hover-effect": !item.outlined,
+            //         "hover-bg-none": !item.outlined,
+            //       })}
+            //       sx={{
+            //         position: "relative",
+            //         bgcolor: item.outlined ? "grey.100" : "transparent",
+            //       }}
+            //     >
+            //       {item.title}
+            //     </Button>
+            //   </NextLink>
+            // ),
           )}
         </StyledNavLinks>
       </Container>
